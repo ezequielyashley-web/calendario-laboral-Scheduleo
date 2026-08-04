4f8a6fbed9a4efa3518655005737b25907f655ee
ezequielyashley-web <ezequielyashley@web.com>
2026-08-04 15:54:38 +0200

    Header: logo agrandado a 64px de alto (44px seguia viendose pequeno)

--- components/desktop/DesktopLayout.tsx
+++ components/desktop/DesktopLayout.tsx
@@ -584,7 +584,7 @@ export default function DesktopLayout({ children }: { children: React.ReactNode 
                 🔔
               </div>
             )}
-            <img src="/design-system/icons/scheduleo-logo-horizontal.png" alt="Scheduleo" style={{ height:44, width:"auto" }} />
+            <img src="/design-system/icons/scheduleo-logo-horizontal.png" alt="Scheduleo" style={{ height:64, width:"auto" }} />
           </div>
         </header>
         <BetaBanner />
