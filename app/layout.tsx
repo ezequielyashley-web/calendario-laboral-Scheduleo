afd49d10e1fbf9104e0f34c77428c5d62ef8d5f2
ezequielyashley-web <ezequielyashley@web.com>
2026-08-05 15:14:50 +0200

    Favicon: nueva version con circulo blanco y trazo cromado azul, S ocupando el maximo espacio

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const dynamic = 'force-dynamic'
 
 export async function generateMetadata(): Promise<Metadata> {
   let nombreEmpresa = 'Scheduleo'
-  let faviconUrl = '/design-system/icons/scheduleo-symbol.png'
+  let faviconUrl = '/design-system/icons/scheduleo-favicon.png'
   try {
     const { prisma } = await import('@/lib/prisma')
     const empresa: any = await prisma.empresa.findUnique({ where: { id: 'empresa-001' } })
